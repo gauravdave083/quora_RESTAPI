71494f99870bbce26aab5615c17bc394a84fe336
gauravdave083 <gd836398@gmail.com>
2024-08-07 02:20:44 +0530

    second

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.set(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        username: "gauravdave",
+        content: "I love coding"
+    },
+    {
+        username: "Akshat",
+        content: "Perfect practice makes a man perfect"
+    },
+    {
+        username: "Ram",
+        content: "Jai Jai Shree Ram"
+    }
+];
+
+
+app.get('/post', function(req, res){
+    res.render("index.ejs", { posts })
+})
 
 app.listen(port, () => {
     console.log(`listening to port ${port}`)
 });
 
-app.get('/', function(req, res){
-    res.send('Hello World!!')
-})
 
